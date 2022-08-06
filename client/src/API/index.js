6880df44cf8be9eea03aec7e6b1c16254c9fc978
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2022-08-05 17:47:46 -0700

    change url

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: 'http://localhost:5000/'})
+const API = axios.create({baseURL: 'https://patientscheduler.herokuapp.com/'})
 
 export const addAppointment = (added) => API.post('/appointment', added)
 export const getAppointments = () => API.get('/appointment')
